0a92df3c1831376b724457256a051d7b8d9bbbcf
Kittitat Songsakseree <kittitater@gmail.com>
2023-09-21 06:08:02 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // this is server handler
+
+var finalhandler = require("finalhandler");
+var http = require("http");
+var serveStatic = require("serve-static");
+
+// Serve up public/ftp folder
+var serve = serveStatic("src", { index: ["dashboard.html", "dashboard.html"] });
+
+// Create server
+var server = http.createServer(function onRequest(req, res) {
+  serve(req, res, finalhandler(req, res));
+});
+
+// Listen
+server.listen(8080);
